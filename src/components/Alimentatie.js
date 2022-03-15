20fa7d4f48e455aecc1f705df509dbabcf8889f3
andycarl2014 <innocentskz42@gmail.com>
2022-03-15 18:31:34 +0200

    2

--- src/components/Alimentatie.js
+++ src/components/Alimentatie.js
@@ -1,5 +1,13 @@
-import React, { useState } from 'react';
-import { Form, Button, Select, DatePicker, Space } from 'antd';
+import React, { useState, useEffect } from 'react';
+import {
+  Form,
+  Button,
+  Select,
+  DatePicker,
+  Space,
+  Input,
+  InputNumber,
+} from 'antd';
 import {
   VEGETABLES,
   FRUITS,
@@ -21,7 +29,7 @@ const Alimentatie = () => {
   const [form] = Form.useForm();
   const [type, setType] = useState('');
   const [startdate, setStartdate] = useState(null);
-  const [enddate, setEnddate] = useState(null);
+  const [typeChildren, setTypeChildren] = useState([]);
   const listTypes = [
     'VEGETABLES',
     'FRUITS',
@@ -58,88 +66,155 @@ const Alimentatie = () => {
           },
         }
       : null;
+  const capitalize = (string) => {
+    string = string.toLowerCase();
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
   const children = [];
 
   for (let i = 0; i < listTypes.length; i++) {
-    children.push(<Option key={listTypes[i]}>{listTypes[i]}</Option>);
+    children.push(
+      <Option key={listTypes[i]}>{capitalize(listTypes[i])}</Option>,
+    );
   }
-  let typeChildren = [];
+
   const handleChangeType = (value) => {
     switch (value) {
       case 'VEGETABLES':
+        setType('VEGETABLES');
+        break;
+      case 'FRUITS':
+        setType('FRUITS');
+        break;
+      case 'BREADS':
+        setType('BREADS');
+        break;
+      case 'PASTRIES':
+        setType('PASTRIES');
+        break;
+      case 'CONDIMENTS':
+        setType('CONDIMENTS');
+        break;
+      case 'CURDS':
+        setType('CURDS');
+        break;
+      case 'HERBS':
+        setType('HERBS');
+        break;
+      case 'SPICES':
+        setType('SPICES');
+        break;
+      case 'MIXTURES':
+        setType('MIXTURES');
+        break;
+      case 'COCKTAILS':
+        setType('COCKTAILS');
+        break;
+      case 'TEAS':
+        setType('TEAS');
+        break;
+      case 'ALCOHOL':
+        setType('ALCOHOL');
+        break;
+      case 'MEAT':
+        setType('MEAT');
+        break;
+      default:
+    }
+  };
+  useEffect(() => {
+    const children = [];
+    switch (type) {
+      case 'VEGETABLES':
         for (let i = 0; i < VEGETABLES.length; i++) {
-          typeChildren.push(
-            <Option key={VEGETABLES[i]}>{VEGETABLES[i]}</Option>,
+          children.push(
+            <Option key={VEGETABLES[i]}>{capitalize(VEGETABLES[i])}</Option>,
           );
-          console.log(typeChildren[5]);
         }
+
         break;
       case 'FRUITS':
         for (let i = 0; i < FRUITS.length; i++) {
-          typeChildren.push(<Option key={FRUITS[i]}>{FRUITS[i]}</Option>);
+          children.push(
+            <Option key={FRUITS[i]}>{capitalize(FRUITS[i])}</Option>,
+          );
         }
         break;
       case 'BREADS':
         for (let i = 0; i < BREADS.length; i++) {
-          typeChildren.push(<Option key={BREADS[i]}>{BREADS[i]}</Option>);
+          children.push(
+            <Option key={BREADS[i]}>{capitalize(BREADS[i])}</Option>,
+          );
         }
         break;
       case 'PASTRIES':
         for (let i = 0; i < PASTRIES.length; i++) {
-          typeChildren.push(<Option key={PASTRIES[i]}>{PASTRIES[i]}</Option>);
+          children.push(
+            <Option key={PASTRIES[i]}>{capitalize(PASTRIES[i])}</Option>,
+          );
         }
         break;
       case 'CONDIMENTS':
         for (let i = 0; i < CONDIMENTS.length; i++) {
-          typeChildren.push(
-            <Option key={CONDIMENTS[i]}>{CONDIMENTS[i]}</Option>,
+          children.push(
+            <Option key={CONDIMENTS[i]}>{capitalize(CONDIMENTS[i])}</Option>,
           );
         }
         break;
       case 'CURDS':
         for (let i = 0; i < CURDS.length; i++) {
-          typeChildren.push(<Option key={CURDS[i]}>{CURDS[i]}</Option>);
+          children.push(<Option key={CURDS[i]}>{capitalize(CURDS[i])}</Option>);
         }
         break;
       case 'HERBS':
         for (let i = 0; i < HERBS.length; i++) {
-          typeChildren.push(<Option key={HERBS[i]}>{HERBS[i]}</Option>);
+          children.push(<Option key={HERBS[i]}>{capitalize(HERBS[i])}</Option>);
         }
         break;
       case 'SPICES':
         for (let i = 0; i < SPICES.length; i++) {
-          typeChildren.push(<Option key={SPICES[i]}>{SPICES[i]}</Option>);
+          children.push(
+            <Option key={SPICES[i]}>{capitalize(SPICES[i])}</Option>,
+          );
         }
         break;
       case 'MIXTURES':
         for (let i = 0; i < MIXTURES.length; i++) {
-          typeChildren.push(<Option key={MIXTURES[i]}>{MIXTURES[i]}</Option>);
+          children.push(
+            <Option key={MIXTURES[i]}>{capitalize(MIXTURES[i])}</Option>,
+          );
         }
         break;
       case 'COCKTAILS':
         for (let i = 0; i < COCKTAILS.length; i++) {
-          typeChildren.push(<Option key={COCKTAILS[i]}>{COCKTAILS[i]}</Option>);
+          children.push(
+            <Option key={COCKTAILS[i]}>{capitalize(COCKTAILS[i])}</Option>,
+          );
         }
         break;
       case 'TEAS':
         for (let i = 0; i < TEAS.length; i++) {
-          typeChildren.push(<Option key={TEAS[i]}>{TEAS[i]}</Option>);
+          children.push(<Option key={TEAS[i]}>{capitalize(TEAS[i])}</Option>);
         }
         break;
       case 'ALCOHOL':
         for (let i = 0; i < ALCOHOL.length; i++) {
-          typeChildren.push(<Option key={ALCOHOL[i]}>{ALCOHOL[i]}</Option>);
+          children.push(
+            <Option key={ALCOHOL[i]}>{capitalize(ALCOHOL[i])}</Option>,
+          );
         }
         break;
       case 'MEAT':
         for (let i = 0; i < MEAT.length; i++) {
-          typeChildren.push(<Option key={MEAT[i]}>{MEAT[i]}</Option>);
+          children.push(<Option key={MEAT[i]}>{capitalize(MEAT[i])}</Option>);
         }
         break;
       default:
-        typeChildren.push(<Option key={0}>Null</Option>);
+        children.push(<Option key={0}>Please Select The Type First</Option>);
     }
-  };
+    setTypeChildren(children);
+  }, [type, setTypeChildren]);
+
   const handleChangeStartDate = (value) => {
     setStartdate(value);
   };
@@ -161,21 +236,25 @@ const Alimentatie = () => {
         </Select>
       </Form.Item>
       <Form.Item label='Aliment'>
-        <Select
-          style={{ width: '100%' }}
-          showSearch
-          onChange={handleChangeType}>
+        <Select style={{ width: '100%' }} showSearch>
           {typeChildren}
         </Select>
       </Form.Item>
       <Form.Item label='Data Si Ora'>
         <Space direction='horizontal'>
           <DatePicker
+            showTime={{ format: 'HH:mm' }}
             placeholder='Select Start Date'
             value={startdate}
             onChange={handleChangeStartDate}
           />
         </Space>
+      </Form.Item>
+      <Form.Item label='Cantitate'>
+        <InputNumber />
+      </Form.Item>
+      <Form.Item label='Gramaj'>
+        <InputNumber />
       </Form.Item>
       <Form.Item {...buttonItemLayout}>
         <Button type='primary' onClick={handleClickSubmit}>
